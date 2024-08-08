91142cfd4006d4a042587912a4eef02c08c3c50b
EEJefferson <161738186+EEJefferson@users.noreply.github.com>
2024-08-08 16:51:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
- let stack =[];  
-let brObj = {};
-  bracketsConfig.map(item => { brObj[item[0]] = item[1] });
-  if(str.length%2!==0){
-    return false
-  } else{
-  for(let i = 0; i<str.length;i++){
-    let cur = str[i];
-if ( Object.keys(brObj).includes(cur) ) { 
-        if ((cur === stack[stack.length - 1]) && (brObj[cur] == cur)) { stack.pop() }
-        else stack.push(cur) }
-      else if ( Object.values(brObj).includes(cur) ) {
-        if (brObj[stack.pop()] !== cur) return false
-        }   
+  const openBrackets = bracketsConfig.map(pair => pair[0]);
+  const closeBrackets = bracketsConfig.map(pair => pair[1]);
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    const openIndex = openBrackets.indexOf(char);
+
+    if (openIndex !== -1) {
+      stack.push(openIndex);
+    } else {
+      const closeIndex = closeBrackets.indexOf(char);
+
+      if (closeIndex !== -1 && stack.length > 0 && stack[stack.length - 1] === closeIndex) {
+        stack.pop();
+      } else {
+        return false;
       }
-    return (stack.length === 0)
+    }
   }
-}
+
+  return stack.length === 0;
+};
